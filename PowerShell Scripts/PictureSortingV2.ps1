9e1fc769c7762f877b54cb07aeccdf1a18883a5e
Frank Syhler <frank.syhler@nimbleit.dk>
2023-01-10 12:58:40 +0100

    rettelser

--- PowerShell Scripts/PictureSortingV2.ps1
+++ PowerShell Scripts/PictureSortingV2.ps1
@@ -1,9 +1,13 @@
-$TargetFolder = "C:\Users\FrankSyhler\Downloads"
-$SortedFolder = "C:\Temp"
+#Move pictures from this folder
+$FromPath = "D:\Billeder\Nye billeder til sortering"
+#Target folder to place files
+$ToPath = "C:\Temp\New folder"
+$FileType = "*.jpeg"
 
-function Sort-Files ($FromPath, $ToPath, $FileType){
-    Get-ChildItem "$FromPath\*" -Include $FileType -Recurse | foreach {
-        $FullDate = $_.CreationTime
+#function Sort-Files ($FromPath, $ToPath, $FileType){
+    $AllFiles = Get-ChildItem "$FromPath\*" -Include $FileType -Recurse 
+    $AllFiles | foreach {
+        $FullDate = $_.LastWriteTime
         $YearDate = Get-Date $FullDate -Format yyyy
         $MonthDate = Get-Date $FullDate -Format MM
         $DesPathForFolder = "$ToPath\$YearDate\$MonthDate"
@@ -17,28 +21,63 @@ function Sort-Files ($FromPath, $ToPath, $FileType){
         {
             if (test-path $DesPathForFolder)
             { 
-                Write-Output "Flytter filen $FileName"
-                move-item $_.fullname $DesPathForFolder 
+                Write-Output "Kopiere filen $FileName"
+                Copy-Item $_.fullname $DesPathForFolder 
             }
             else
             {
                 Write-Output "Orpetter mappe $DesPathForFolder"
-                new-item -ItemType directory -Path $DesPathForFolder
-                Write-Output "Flytter filen $FileName"
-                move-item $_.fullname $DesPathForFolder
+                New-Item -ItemType directory -Path $DesPathForFolder
+                Write-Output "Kopiere filen $FileName"
+                Copy-Item $_.fullname $DesPathForFolder
             }
         }
     }
-}
+#}
 
 Sort-Files -FromPath $TargetFolder -ToPath $SortedFolder -FileType "*.jpeg"
 Sort-Files -FromPath $TargetFolder -ToPath $SortedFolder -FileType "*.jpg"
 Sort-Files -FromPath $TargetFolder -ToPath $SortedFolder -FileType "*.png"
 
-<#-------------------------------------------------------------------------------
-$Test = Get-ChildItem "$TargetFolder\*" -Recurse
-$FullDate = $Test.CreationTime
+#-------------------------------------------------------------------------------
+$Test = Get-ChildItem "C:\Temp\New folder\2022\11\IMG_7116.JPEG"
+$FullDate = $Test.LastWriteTime
 $YearDate = Get-Date $FullDate -Format yyyy
 $MonthDate = Get-Date $FullDate -Format MM
 Get-Item -Path "$TargetFolder\IMG_1481.jpg" | Get-Member
-#>
+
+
+$AllFiles.LastWriteTime
+$AllFiles.CreationTime
+
+
+
+try
+{
+    $path = "C:\Temp\IMG_0116.JPEG"
+    $pathModified = "C:\Temp\IMG_0116_MODIFIED.JPEG"
+
+    Add-Type -AssemblyName System.Drawing
+    $bitmap = New-Object System.Drawing.Bitmap($path)
+    $propertyItem = $bitmap.GetPropertyItem(36867) 
+    $bytes = $propertyItem.Value 
+    $string = [System.Text.Encoding]::ASCII.GetString($bytes) 
+    $dateTime = [DateTime]::ParseExact($string,"yyyy:MM:dd HH:mm:ss`0",$Null)
+   
+    $dateTimeModified = $dateTime.AddDays(1) # Set new date here
+    $stringModified = $dateTimeModified.ToString("yyyy:MM:dd HH:mm:ss`0",$Null)
+    $bytesModified = [System.Text.Encoding]::ASCII.GetBytes($stringModified) 
+
+    $propertyItem.Value = $bytesModified
+    $bitmap.SetPropertyItem($propertyItem)
+    $bitmap.Save($pathModified)
+}
+finally
+{
+    $bitmap.Dispose()
+}
+
+$Test = Get-ChildItem $pathModified
+
+$Test.LastWriteTime
+$Test.CreationTime
